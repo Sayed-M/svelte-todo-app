e4d7e3f2c6a8286f5955de9f1a4da523a67070ce
Sayed Mohamed <develoopersayed@gmail.com>
2023-02-12 22:04:54 +0200

    break words

--- src/lib/components/folder.svelte
+++ src/lib/components/folder.svelte
@@ -6,7 +6,7 @@
 
 </script>
 
-<li class="flex mb-2"><a href="/folders/{folder.id}" class="grow no-underline capitalize  rounded bg-slate-600 hover:bg-sky-400 text-white break-words p-2" class:active={folder.id === $page.params.folderId}>{folder.name}</a></li>
+<li class="flex mb-2"><a href="/folders/{folder.id}" class="grow no-underline capitalize break-all rounded bg-slate-600 hover:bg-sky-400 text-white break-words p-2" class:active={folder.id === $page.params.folderId}>{folder.name}</a></li>
 
 <style lang="postcss">
     .active { 
