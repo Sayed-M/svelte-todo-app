74571fe3f615b55605e244cb5073f9a1a70f913f
Sayed Mohamed <develoopersayed@gmail.com>
2023-02-12 16:48:07 +0200

    add active class to the active folder

--- src/lib/components/folder.svelte
+++ src/lib/components/folder.svelte
@@ -1,8 +1,15 @@
 <script lang="ts">
     import type { IFolder } from "../models/models";
-
+	import { page } from '$app/stores';
+    
     export let folder: IFolder;
 
 </script>
 
-<li><a href="/{folder.id}" class="text-primary break-words">{folder.name}</a></li>
+<li><a href="/{folder.id}" class="text-black break-words" class:active={folder.id === $page.params.id}>{folder.name}</a></li>
+
+<style lang="postcss">
+    .active { 
+        @apply text-primary;
+    }
+</style>
