e9ab2c534249132f7df474b87dbdeb08c8e7d19f
Sayed Mohamed <develoopersayed@gmail.com>
2023-02-13 21:06:29 +0200

    delete folders

--- src/lib/components/folder.svelte
+++ src/lib/components/folder.svelte
@@ -1,12 +1,43 @@
 <script lang="ts">
+    import { goto } from '$app/navigation';
     import type { IFolder } from "../models/models";
 	import { page } from '$app/stores';
+	import { folders } from "$lib/stores/folders";
     
     export let folder: IFolder;
 
+    let deleteFolder = () => {
+        if (folder.todos.length) {
+            let discardDeleting = confirm(`You have ${folder.todos.length} Todo(s) in this folder, Do you really want to delete?`);
+            if (discardDeleting) {
+                confirmDelete();
+            }
+        } else {
+            let discardDeleting = confirm(`Do you really want to delete?`);
+            if (discardDeleting) {
+                confirmDelete();
+            }
+        }
+        if ($folders.length === 0) {
+            goto(`/`)
+        }
+    }
+
+    let confirmDelete = () => {
+        let updatedFolders = $folders.filter(fd => fd.id !== folder.id)
+        $folders = updatedFolders;
+    }
+
 </script>
 
-<li class="flex mb-2"><a href="/{folder.id}" class="grow no-underline capitalize break-all rounded bg-slate-600 hover:bg-sky-400 text-white break-words p-2" class:active={folder.id === $page.params.folderId}>{folder.name}</a></li>
+<li class="flex mb-2 relative">
+    <a href="/{folder.id}" 
+    class="grow no-underline break-all rounded bg-slate-600 hover:bg-sky-400 text-white break-words p-2" 
+    class:active={folder.id === $page.params.folderId}>{folder.name}</a>
+    <button class="text-white absolute right-2 top-[10px] font-mono" 
+    title="Delete {folder.name}"
+    on:click={deleteFolder}>X</button>
+</li>
 
 <style lang="postcss">
     .active { 
