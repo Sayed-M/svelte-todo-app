a2d52e99ef0be76060ce15bf09a33efcb44c461f
Sayed Mohamed <develoopersayed@gmail.com>
2023-02-16 06:13:11 +0200

    fix "Cannot read properties of undefined" error when deleting a folder

--- src/lib/components/TodosPage.svelte
+++ src/lib/components/TodosPage.svelte
@@ -11,22 +11,22 @@
 </script>
 
 <svelte:head>
-	<title>{folder.name} Todos</title>
+	<title>{folder?.name} Todos</title>
 </svelte:head>
 
 <section class="pt-6">
 	<div class="flex justify-between gap-4">
 		<div class="flex flex-col grow min-w-[250px]">
-			<h3 class="text-xl font-bold mb-4 capitalize break-all">{folder.name}</h3>
-			<Tabs folderId={folder.id} 
+			<h3 class="text-xl font-bold mb-4 capitalize break-all">{folder?.name}</h3>
+			<Tabs folderId={folder?.id} 
                 {activeTab} 
-				activeTodosCount={folder.activeTodos.length} 
-				completedTodosCount={folder.completedTodos.length}  />
-			<Todos todos={activeTab === TodosTabs.Active ? folder.activeTodos : folder.completedTodos} 
+				activeTodosCount={folder?.activeTodos.length} 
+				completedTodosCount={folder?.completedTodos.length}  />
+			<Todos todos={activeTab === TodosTabs.Active ? folder?.activeTodos : folder?.completedTodos} 
 			{folder} 
             {activeTab} 
-			activeTodosCount={folder.activeTodos.length} 
-			completedTodosCount={folder.completedTodos.length}  />
+			activeTodosCount={folder?.activeTodos.length} 
+			completedTodosCount={folder?.completedTodos.length}  />
 		</div>
 		{#if activeTab === TodosTabs.Active}
             <AddTodo {folder}></AddTodo>
