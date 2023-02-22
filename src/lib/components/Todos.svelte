490624665f12e60e8d8e957079193aa4c96cf189
Sayed Mohamed <develoopersayed@gmail.com>
2023-02-22 15:54:18 +0200

    move myDerivedStore to stores file

--- src/lib/components/Todos.svelte
+++ src/lib/components/Todos.svelte
@@ -1,31 +1,11 @@
 <script lang="ts">
 	import Todo from "$lib/components/Todo.svelte";
 	import { TodosTabs } from "$lib/enums/enums";
-	import { derived } from "svelte/store";
-	import { folders } from "$lib/stores/folders";
-	import { page } from "$app/stores";
+    import { myDerivedStore } from "$lib/stores/folders";
 
     export let activeTab: TodosTabs = TodosTabs.Active;
 
-    export const myDerivedStore =derived([folders, page], ([$folders, $page]) => {
-        const folder = $folders.find(folder => folder.id === $page.params.folderId);
-        let todos;
-        let activeTodosCount;
-        let completedTodosCount;
-        if ($page.url.pathname.includes('completed')) {
-            todos = folder?.completedTodos;
-            completedTodosCount = folder?.completedTodos.length;
-        } else {
-            todos = folder?.activeTodos;
-            activeTodosCount = folder?.activeTodos.length;
-        }
-        return {
-            folder,
-            todos,
-            activeTodosCount,
-            completedTodosCount
-        }
-    });
+
 </script>
 
 <div>
--- src/lib/stores/folders.ts
+++ src/lib/stores/folders.ts
@@ -1,4 +1,25 @@
-import { writable } from "svelte/store";
+import { page } from "$app/stores";
+import { derived, writable } from "svelte/store";
 import type { IFolder } from "../models/models";
 
 export const folders = writable<IFolder[]>([]);
+
+export const myDerivedStore =derived([folders, page], ([$folders, $page]) => {
+    const folder = $folders.find(folder => folder.id === $page.params.folderId);
+    let todos;
+    let activeTodosCount;
+    let completedTodosCount;
+    if ($page.url.pathname.includes('completed')) {
+        todos = folder?.completedTodos;
+        completedTodosCount = folder?.completedTodos.length;
+    } else {
+        todos = folder?.activeTodos;
+        activeTodosCount = folder?.activeTodos.length;
+    }
+    return {
+        folder,
+        todos,
+        activeTodosCount,
+        completedTodosCount
+    }
+});
